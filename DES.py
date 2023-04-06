d82efc2b3703cf5d4f9153ddc7344b8d753b7868
AdamCsiki <112400969+AdamCsiki@users.noreply.github.com>
2023-04-06 14:16:32 +0300

    Prototype of DES encryptor

---
+++ DES.py
@@ -0,0 +1,15 @@
+from step_one import *
+from step_two import *
+
+def DES_encrypt(msg: hex, key: hex) -> hex:
+    return step_two(msg, step_one(key))
+
+def main():
+    key = "133457799BBCDFF1"
+    msg = "0123456789ABCDEF"
+    print("MESSAGE: ", msg)
+    print("KEY: ", key, "\n---------------------------------")
+    print("ENCRYPTED: ", DES_encrypt(msg, key))
+    
+if __name__ == "__main__":
+main()
